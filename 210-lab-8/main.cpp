7eedfd61cacc26984cac4263b8b9e2b9888ffa5a
leahbgithub <leahchaku@gmail.com>
2024-09-22 13:07:10 -0700

    Initial Commit

---
+++ 210-lab-8/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  210-lab-8
+//
+//  Created by Leah Balakrishnan on 9/22/24.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Testing..." << std::endl;
+    return 0;
+}
